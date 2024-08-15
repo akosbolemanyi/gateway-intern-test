048f08072213a27225884b7d47b887438b34e47b
Bolemányi Ákos <bolemanyi.akos@metrix.co.hu>
2024-08-15 09:41:05 +0200

    Defined: schema for movies.

---
+++ src/movie/schemas/movie.schema.ts
@@ -0,0 +1,20 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+
+@Schema({
+    timestamps: true,
+})
+export class Movie extends Document {
+    @Prop({ required: true })
+    title: string;
+
+    @Prop({ required: true })
+    description: string;
+
+    @Prop({ required: true })
+    coverImage: string;
+
+    @Prop({ default: false })
+    isWinner: boolean;
+}
+
+export const MovieSchema = SchemaFactory.createForClass(Movie);
