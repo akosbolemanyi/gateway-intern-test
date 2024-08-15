494c4ce7612cabdc4e16d459ab26c92e637cd7f9
Bolemányi Ákos <bolemanyi.akos@metrix.co.hu>
2024-08-15 14:59:39 +0200

    Update: added faker via nestjs-seeder

--- src/movie/schemas/movie.schema.ts
+++ src/movie/schemas/movie.schema.ts
@@ -1,18 +1,25 @@
 import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Factory} from "nestjs-seeder";
+
+export type MovieDocument = Movie & Document;
 
 @Schema({
     timestamps: true,
 })
 export class Movie {
+    @Factory(faker => faker.lorem.words(2))
     @Prop({ required: true })
     title: string;
 
+    @Factory(faker => faker.lorem.words(10))
     @Prop({ default: "" })
     description: string;
 
+    @Factory(faker => faker.image.url())
     @Prop({ default: "" })
     coverImage: string;
 
+    @Factory(faker => faker.color.human() === 'red')
     @Prop({ default: false })
     isWinner: boolean;
 }
