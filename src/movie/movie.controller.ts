1699172f7f0217a9eedb96d6e47ab65e9bde3c6c
Bolemányi Ákos <bolemanyi.akos@metrix.co.hu>
2024-08-16 10:27:44 +0200

    Added: @Req instead of @Query.

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,51 +1,56 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post, Query, Req} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { Movie } from './schemas/movie.schema';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
-import { Query as ExpressQuery } from 'express-serve-static-core';
+import { Request } from 'express';
 
 @Controller('movies')
 export class MovieController {
-    constructor(private movieService: MovieService) {}
+  constructor(private movieService: MovieService) {}
 
-    @Get()
-    async getAllMovies(@Query() query: ExpressQuery): Promise<Movie[]> {
-        return this.movieService.findAll(query);
-    }
+  @Get()
+  async getAllMovies(@Req() req: Request): Promise<Movie[]> {
+    return this.movieService.findAll(req);
+  }
 
-    @Get('winners')
-    async getAllWinners(): Promise<Movie[]> {
-        return this.movieService.findWinners();
-    }
+  @Get('winners')
+  async getAllWinners(): Promise<Movie[]> {
+    return this.movieService.findWinners();
+  }
 
-    @Get(':id')
-    async getMovie(
-        @Param('id') id: string,
-    ): Promise<Movie> {
-        return this.movieService.findById(id);
-    }
+  @Get(':id')
+  async getMovie(@Param('id') id: string): Promise<Movie> {
+    return this.movieService.findById(id);
+  }
 
-    @Post(['new', ':id/new'])
-    async createMovieFromDetailPage(
-        @Param('id') id: string,
-        @Body() createMovieDto: CreateMovieDto,
-    ): Promise<Movie> {
-        return this.movieService.create(createMovieDto);
-    }
+  @Post(['new', ':id/new'])
+  async createMovieFromDetailPage(
+    @Param('id') id: string,
+    @Body() createMovieDto: CreateMovieDto,
+  ): Promise<Movie> {
+    return this.movieService.create(createMovieDto);
+  }
 
-    @Delete(':id')
-    async deleteMovie(
-        @Param('id') id: string,
-    ): Promise<Movie> {
-        return this.movieService.deleteById(id);
-    }
+  @Delete(':id')
+  async deleteMovie(@Param('id') id: string): Promise<Movie> {
+    return this.movieService.deleteById(id);
+  }
 
-    @Patch(':id')
-    async updateMovie(
-        @Param('id') id: string,
-        @Body() updateMovieDto: UpdateMovieDto,
-    ): Promise<Movie> {
-        return this.movieService.updateById(id, updateMovieDto);
-    }
+  @Patch(':id')
+  async updateMovie(
+    @Param('id') id: string,
+    @Body() updateMovieDto: UpdateMovieDto,
+  ): Promise<Movie> {
+    return this.movieService.updateById(id, updateMovieDto);
+  }
 }
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,68 +1,111 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Movie } from './schemas/movie.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
-import {UpdateMovieDto} from "./dto/update-movie.dto";
-import {CreateMovieDto} from "./dto/create-movie.dto";
-import { Query } from 'express-serve-static-core';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { Request } from 'express';
 
 @Injectable()
 export class MovieService {
-    constructor(
-        @InjectModel(Movie.name)
-        private movieModel: mongoose.Model<Movie>,
-    ) {}
+  constructor(
+    @InjectModel(Movie.name) private readonly movieModel: mongoose.Model<Movie>,
+  ) {}
 
-    async findAll(query: Query): Promise<Movie[]> {
-        const limit = 5;
-        const currentPage = Number(query.page) ?? 1;
-        const skip = limit * (currentPage - 1);
+  async findAll(req: Request): Promise<Movie[]> {
+    const limit = 5;
+    const currentPage = Number(req.query.page) || 1;
+    const skip = limit * (currentPage - 1);
 
-        const search = query.keyword ? {
-            title: {
-                $regex: query.keyword,
-                $options: 'i'
-            },
-        } : {}
-        return this.movieModel.find(
-            { ...search })
-            .limit(limit)
-            .skip(skip)
-            .exec();
-    }
-
-    async findWinners(): Promise<Movie[]> {
-        return this.movieModel.find({ winner: true }).exec();
-    }
-
-    async findById(id: string): Promise<Movie> {
-        const movie = await this.movieModel.findById(id).exec();
-        if (!movie) {
-            throw new NotFoundException(`Movie with id ${id} not found!`);
+    const search = req.query.search
+      ? {
+          title: {
+            $regex: req.query.search,
+            $options: 'i',
+          },
         }
-        return movie;
+      : {};
+
+    const sort: { [key: string]: 1 | -1 } = {
+      isWinner: -1,
+      title: 1,
+    };
+
+    const sortParams = req.query.sort?.toString().split(',') || [];
+    sortParams.forEach((param) => {
+      if (param === 'title') {
+        sort['title'] = 1;
+      } else if (param === 'isWinner') {
+        sort['isWinner'] = -1;
+      }
+    });
+
+    return this.movieModel
+      .find(search)
+      .sort(sort)
+      .limit(limit)
+      .skip(skip)
+      .exec();
+  }
+
+  async findWinners(): Promise<Movie[]> {
+    return this.movieModel.find({ isWinner: true }).exec();
+  }
+
+  async findById(id: string): Promise<Movie> {
+    const movie = await this.movieModel.findById(id).exec();
+
+    const isValid = mongoose.isValidObjectId(id);
+
+    if (!isValid) {
+      throw new BadRequestException('Please give a valid id!');
     }
 
-    async create(movie: CreateMovieDto): Promise<Movie> {
-        return this.movieModel.create(movie);
+    if (!movie) {
+      throw new NotFoundException(`Movie with id ${id} not found!`);
+    }
+    return movie;
+  }
+
+  async create(movie: CreateMovieDto): Promise<Movie> {
+    return this.movieModel.create(movie);
+  }
+
+  async deleteById(id: string): Promise<Movie> {
+    const movie = await this.movieModel.findByIdAndDelete(id).exec();
+
+    const isValid = mongoose.isValidObjectId(id);
+
+    if (!isValid) {
+      throw new BadRequestException('Please give a valid id!');
     }
 
-    async deleteById(id: string): Promise<Movie> {
-        const movie = await this.movieModel.findByIdAndDelete(id).exec();
-        if (!movie) {
-            throw new NotFoundException(`Movie with id ${id} not found!`);
-        }
-        return movie;
+    if (!movie) {
+      throw new NotFoundException(`Movie with id ${id} not found!`);
+    }
+    return movie;
+  }
+
+  async updateById(id: string, movie: UpdateMovieDto): Promise<Movie> {
+    const isValid = mongoose.isValidObjectId(id);
+
+    if (!isValid) {
+      throw new BadRequestException('Please give a valid id!');
     }
 
-    async updateById(id: string, movie: UpdateMovieDto): Promise<Movie> {
-        const updatedMovie = await this.movieModel.findByIdAndUpdate(id, movie, {
-            new: true,
-            runValidators: true,
-        }).exec();
-        if (!updatedMovie) {
-            throw new NotFoundException(`Movie with id ${id} not found!`);
-        }
-        return updatedMovie;
+    const updatedMovie = await this.movieModel
+      .findByIdAndUpdate(id, movie, {
+        new: true,
+        runValidators: true,
+      })
+      .exec();
+    if (!updatedMovie) {
+      throw new NotFoundException(`Movie with id ${id} not found!`);
     }
+    return updatedMovie;
+  }
 }
