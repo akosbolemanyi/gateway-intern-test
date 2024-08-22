db5462b04fd746797f7664f5f32e7aa4a2b52b67
Bolemányi Ákos <bolemanyi.akos@metrix.co.hu>
2024-08-22 14:31:59 +0200

    New: validation pipe.

---
+++ src/utils/pipes/json-transform-validation.pipe.ts
@@ -0,0 +1,38 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+import { validateSync, ValidationError } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+
+@Injectable()
+export class TransformAndValidatePipe implements PipeTransform {
+  constructor(private readonly dtoClass: any) {}
+
+  transform(value: any): any {
+    if (typeof value === 'string') {
+      try {
+        value = JSON.parse(value);
+      } catch (error) {
+        throw new BadRequestException('Invalid JSON format!');
+      }
+    } else if (typeof value !== 'object' || value === null) {
+      throw new BadRequestException('Invalid data format!');
+    }
+
+    const dtoObject = plainToInstance(this.dtoClass, value);
+
+    const errors: ValidationError[] = validateSync(dtoObject);
+
+    if (errors.length > 0) {
+      const errorMessages = errors.map((error) => ({
+        property: error.property,
+        constraints: error.constraints,
+      }));
+
+      throw new BadRequestException({
+        message: 'Validation failed.',
+        errors: errorMessages,
+      });
+    }
+
+    return dtoObject;
+  }
+}
