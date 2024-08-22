655bd68289829c968e15aacb81d78ee6c8171b2a
Bolemányi Ákos <bolemanyi.akos@metrix.co.hu>
2024-08-22 14:31:18 +0200

    Registered: ExcpetionFilter.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { BadRequestExceptionFilter } from './utils/filters/bad-request-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new BadRequestExceptionFilter());
 
   await app.listen(3000);
 }
