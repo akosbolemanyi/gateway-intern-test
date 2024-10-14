5bb62faef4e1e2e577ce7840663803165f3227f2
Bolemányi Ákos <bolemanyi.akos@metrix.co.hu>
2024-10-14 09:37:53 +0200

    Created service function that returns HTML file given from multipart/form-data.

--- src/html-analyzer/html-analyzer.service.ts
+++ src/html-analyzer/html-analyzer.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HtmlAnalyzerService {}
+export class HtmlAnalyzerService {
+  async validate(id: string, file: Express.Multer.File): Promise<string> {
+    console.log('Ez az ID: ' + id);
+    return file.buffer.toString('utf8');
+  }
+}
