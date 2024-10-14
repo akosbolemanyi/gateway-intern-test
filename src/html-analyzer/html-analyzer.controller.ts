794b1f1686072eef7011263057335883e9eed996
Bolemányi Ákos <bolemanyi.akos@metrix.co.hu>
2024-10-14 09:36:58 +0200

    Calibrated POST endpoint for receiving HTML file and humanID.

--- src/html-analyzer/html-analyzer.controller.ts
+++ src/html-analyzer/html-analyzer.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { HtmlAnalyzerService } from './html-analyzer.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { HtmlFileValidationPipe } from '../utils/pipes/html-validation.pipe';
 
 @Controller('html-analyzer')
-export class HtmlAnalyzerController {}
+export class HtmlAnalyzerController {
+  constructor(private readonly htmlAnalyzerService: HtmlAnalyzerService) {}
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post()
+  async analyzeHtml(
+    @Body('id') id: string,
+    @UploadedFile(HtmlFileValidationPipe) file: Express.Multer.File,
+  ): Promise<string> {
+    return this.htmlAnalyzerService.validate(id, file);
+  }
+}
