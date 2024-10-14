9ee9d1ad91208196b9ae72ce98445b32eb96381a
Bolemányi Ákos <bolemanyi.akos@metrix.co.hu>
2024-10-14 09:53:14 +0200

    Extended service: now can read HTML file lines and give back values from it.

--- src/html-analyzer/html-analyzer.service.ts
+++ src/html-analyzer/html-analyzer.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class HtmlAnalyzerService {
   async validate(id: string, file: Express.Multer.File): Promise<string> {
     console.log('Ez az ID: ' + id);
-    return file.buffer.toString('utf8');
+    const content = file.buffer.toString('utf8');
+    const $ = cheerio.load(content);
+    return $('table tbody tr')
+      .filter((i, el) => {
+        return $(el).find('td').first().text().trim() === 'Ár';
+      })
+      .find('td')
+      .last()
+      .text()
+      .trim();
   }
 }
